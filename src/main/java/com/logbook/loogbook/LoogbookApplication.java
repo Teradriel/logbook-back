5fa796062e23804284726d8cdd03dcd3be24fb05
Luca <luca.terzariol@outlook.com>
2022-08-30 22:04:02 +0200

    minor fixes

--- src/main/java/com/logbook/loogbook/LoogbookApplication.java
+++ src/main/java/com/logbook/loogbook/LoogbookApplication.java
@@ -4,12 +4,13 @@ import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class LoogbookApplication {
 
 	public static void main(String[] args) {
@@ -22,14 +23,19 @@ public class LoogbookApplication {
 		corsConfiguration.setAllowCredentials(true);
 		corsConfiguration.setAllowedOrigins(
 				Arrays.asList("http://localhost:4200"));
-		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin",
+				"Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Request-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
-		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-type", "Accept", "Authorization",
-				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
-		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "PUT", "POST", "DELETE", "OPTIONS"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-type",
+				"Accept", "Authorization",
+				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin",
+				"Access-Control-Allow-Credentials"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "PUT", "POST",
+				"DELETE", "OPTIONS"));
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**",
+				corsConfiguration);
 		return new CorsFilter(urlBasedCorsConfigurationSource);
 	}
 
